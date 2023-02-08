639a74e850d4f69191fdf4c965f8b6f352f90a20
Daniel de Almeida Bernardi <98421842+Danyel1112@users.noreply.github.com>
2023-02-08 11:54:36 -0300

    Commit Teste

---
+++ Themes/js/scripts.js
@@ -0,0 +1,27 @@
+const changeThemeBtn = document.querySelector('#change-theme');
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+// Load light or dark mode
+function loadTheme () {
+    const darkMode = localStorage.getItem("dark")
+
+    if (darkMode) {
+        toggleDarkMode();
+    }
+}
+
+loadTheme ();
+
+changeThemeBtn.addEventListener("change", function () {
+    toggleDarkMode();
+
+    // Save or remove dark mode
+    localStorage.removeItem("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("dark", 1);
+    }
+})
---
+++ timer/script.js
@@ -0,0 +1,5 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
